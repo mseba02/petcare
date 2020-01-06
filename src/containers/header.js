d929d93140e4ec8920e65b9d2e4aec7e12bdce4b
Seba Marius <marius.seba@gmail.com>
2020-01-06 09:44:14 +0200

    d

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,16 +1,51 @@
 // imports
 import React, {Component} from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import './header.css';
+import logo from '../images/logo.png';
 
 
 class Header extends  Component {
+    constructor() {
+        super();
+
+        // state
+        this.state = {
+            registerPopUp: false
+        };
+    }
+    registerUser = (e) => {
+
+    };
     render() {
         return(
-            <section>
+            <header>
+                <form className="register-popup">
+                    <input type="text"  placeholder="Name"/>
+                    <input type="text"  placeholder="User"/>
+                    <input type="text"  placeholder="Password"/>
+                    <button>Register</button>
+                </form>
                <div className="container">
-                   Headerss
+                   <div className="d-flex">
+                       {/* logo */}
+                       <div className="logo-wrap flex-2">
+                           <img src={logo} alt="logo" className="logo"/>
+                       </div>
+                       {/* main navigation */}
+                       <nav className="navigation flex-2 text-right">
+                           <ul className="inline-list">
+                               <li>
+                                   <a href="">Log in</a>
+                               </li>
+                               <li>
+                                   <a href="">Register</a>
+                               </li>
+                           </ul>
+                       </nav>
+                   </div>
                </div>
-            </section>
+            </header>
         )
     }
 };
