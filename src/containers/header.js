d6556d935446e11ae4fa96a63da5d55c70ce9688
Seba Marius <marius.seba@gmail.com>
2020-01-27 17:38:16 +0200

    add classnames

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,30 +1,49 @@
 // imports
 import React, {Component} from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import classNames from 'classnames';
 import './header.css';
 import logo from '../images/logo.png';
 
 
 class Header extends  Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         // state
         this.state = {
-            registerPopUp: false
+            registerPopUp: false,
+            regInputName: '',
+            registerInputLength: '',
         };
     }
-    registerUser = (e) => {
 
+
+
+        handleInputChange = (e, inputField) => {
+        this.setState({
+            [inputField]: e.target.value,
+        })
+    };
+    getInputName = (e) => {
+        this.setState({
+            regInputName: e.target.value,
+            registerInputLength: e.target.value.length
+        })
     };
     render() {
+        // classnames
+        let inputPlaceholder = classNames({
+            register__input: true,
+            register__close: this.state.registerInputLength >= 1
+        });
         return(
             <header>
                 <form className="register-popup">
-                    <h4>Register</h4>
-                    <div>
-                        <label>Name</label>
-                        <input type="text"  placeholder=""/>
+                    <h4 className="register__title">Register</h4>
+                    <div className="position-relative">
+                        <input type="text"  placeholder="" id="name" className={inputPlaceholder} onChange={(e) => this.handleInputChange(e, 'regInputName')} />
+                        <label htmlFor="name" className="register__label">Name</label>
                     </div>
                     <input type="text"  placeholder="User"/>
                     <input type="text"  placeholder="Password"/>
