9cc5fd374f2c214641099bc9a06865d99eda1622
Seba Marius <marius.seba@gmail.com>
2020-03-06 16:55:43 +0200

    update

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,26 @@
 // data
 export const data = {
-
+    animals: [
+        {
+            ab: []
+        }
+    ],
     products: [
-
+        {
+            popular: [
+                {
+                    name: "Mouse bell",
+                    img: require("../assets/img/"),
+                    price: 80,
+                    tag: "New",
+                },
+                {
+                    name: "Rain coat",
+                    img: require("../assets"),
+                    price: 58,
+                    tag: ""
+                }
+            ]
+        }
     ]
 };
